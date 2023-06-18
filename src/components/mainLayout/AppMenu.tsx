b16103a1fc84ec777b446837408d9e69485277ec
Martyna Skup <martyna.skup@yahoo.de>
2023-06-18 18:45:43 +0200

    Update App menu with initial items

--- src/components/mainLayout/AppMenu.tsx
+++ src/components/mainLayout/AppMenu.tsx
@@ -1,27 +1,39 @@
+import React from 'react';
+import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 import {
-  UploadOutlined,
+  ContactsOutlined,
+  HomeOutlined,
+  ProjectOutlined,
   UserOutlined,
-  VideoCameraOutlined,
 } from '@ant-design/icons';
-import React from 'react';
+
+type MenuItem = Required<MenuProps>['items'][number];
 
 const AppMenu = () => {
+  const projectItems: MenuItem[] = [
+    { label: 'Project 1', key: '101' },
+    { label: 'Project 2', key: '102' },
+  ];
+
+  const items: MenuItem[] = [
+    { label: 'Welcome', key: '1', icon: <HomeOutlined /> },
+    { label: 'About me', key: '2', icon: <UserOutlined /> },
+    {
+      label: 'Projects',
+      key: 'sub1',
+      icon: <ProjectOutlined />,
+      children: projectItems,
+    },
+    { label: 'Contact me', key: '4', icon: <ContactsOutlined /> },
+  ];
+
   return (
     <Menu
       theme="dark"
       mode="inline"
-      defaultSelectedKeys={['4']}
-      items={[
-        UserOutlined,
-        VideoCameraOutlined,
-        UploadOutlined,
-        UserOutlined,
-      ].map((icon, index) => ({
-        key: String(index + 1),
-        icon: React.createElement(icon),
-        label: `nav ${index + 1}`,
-      }))}
+      defaultSelectedKeys={['1']}
+      items={items}
     />
   );
 };
