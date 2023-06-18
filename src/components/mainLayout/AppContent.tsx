e0dbdac93ee0b231482681ca211b7f58b13d2701
Martyna Skup <martyna.skup@yahoo.de>
2023-06-18 19:06:44 +0200

    Update margins for app content

--- src/components/mainLayout/AppContent.tsx
+++ src/components/mainLayout/AppContent.tsx
@@ -2,10 +2,10 @@ import { Layout } from 'antd';
 
 const AppContent = () => {
   return (
-    <Layout.Content style={{ margin: '24px 16px 0' }}>
+    <Layout.Content style={{ margin: '16px 16px 0' }}>
       <div
         className="site-layout-background"
-        style={{ padding: 24, minHeight: 360 }}
+        style={{ padding: 24, minHeight: 500 }}
       >
         content
       </div>
