3475c6d426cd2d8338cd668e3d4f862981fb520e
Martyna Skup <martyna.skup@yahoo.de>
2023-06-18 15:08:49 +0200

    Update app footer with contact details

--- src/components/mainLayout/AppFooter.tsx
+++ src/components/mainLayout/AppFooter.tsx
@@ -1,9 +1,42 @@
-import { Layout } from 'antd';
+import { Button, Layout, Space, Typography } from 'antd';
+import {
+  GithubOutlined,
+  LinkedinOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 
 const AppFooter = () => {
   return (
     <Layout.Footer style={{ textAlign: 'center' }}>
-      Created with Ant Design by Martyna Skup
+      <Space size="small">
+        <Typography.Text>
+          Created with Ant Design ©2023 by Martyna Skup (
+          <Button
+            type="link"
+            href="https://github.com/snikerslala"
+            target="_blank"
+            rel="noopener"
+            size="small"
+            icon={<GithubOutlined style={{ color: 'rgba(0, 0, 0, 0.88)' }} />}
+          />
+          <Button
+            type="link"
+            href="https://www.linkedin.com/in/martynaskup/"
+            target="_blank"
+            rel="noopener"
+            size="small"
+            icon={<LinkedinOutlined style={{ color: 'rgba(0, 0, 0, 0.88)' }} />}
+          />
+          <Button
+            type="link"
+            href="mailto: martyna.skup@yahoo.de"
+            rel="noopener"
+            size="small"
+            icon={<MailOutlined style={{ color: 'rgba(0, 0, 0, 0.88)' }} />}
+          />
+          )
+        </Typography.Text>
+      </Space>
     </Layout.Footer>
   );
 };
