8de6ccfc405b61b6c5915dd232273edeae3ea8d7
Martyna Skup <martyna.skup@yahoo.de>
2023-06-19 22:57:12 +0200

    Centralise text in AppContent

--- src/components/mainLayout/AppContent.tsx
+++ src/components/mainLayout/AppContent.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const AppContent = () => {
   return (
     <>
-      <Layout.Content style={{ margin: '16px 16px 0' }}>
+      <Layout.Content style={{ margin: '16px 16px 0', textAlign: 'center' }}>
         <div
           className="site-layout-background"
           style={{ padding: 24, minHeight: 500 }}
