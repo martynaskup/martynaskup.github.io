7d9fe5c63e71244a4271d179170739777ba6fd2f
Martyna Skup <martyna.skup@yahoo.de>
2023-07-07 23:27:15 +0200

    [Budget] Add dummy statistics for incomes and outcomes

---
+++ src/pages/ProjectsPage/Projects/Budget/Budget.styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledStatisticDiv = styled('div')`
+  margin-top: 36px;
+  padding: 18px;
+  background: #ececec;
+`;
--- src/pages/ProjectsPage/Projects/Budget/Budget.tsx
+++ src/pages/ProjectsPage/Projects/Budget/Budget.tsx
@@ -1,5 +1,47 @@
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Col, Row, Statistic, Typography } from 'antd';
+import { StyledStatisticDiv } from './Budget.styles';
+
 function Budget() {
-  return <h1>Budget app</h1>;
+  const appTitle = <Typography.Title level={2}>Budget app</Typography.Title>;
+
+  const statistic = (
+    <StyledStatisticDiv>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card>
+            <Statistic
+              title="Cash in"
+              value={1100.28}
+              precision={2}
+              valueStyle={{ color: '#3f8600' }}
+              prefix={<ArrowUpOutlined />}
+              suffix="€"
+            />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card>
+            <Statistic
+              title="Cash out"
+              value={19.3}
+              precision={2}
+              valueStyle={{ color: '#cf1322' }}
+              prefix={<ArrowDownOutlined />}
+              suffix="€"
+            />
+          </Card>
+        </Col>
+      </Row>
+    </StyledStatisticDiv>
+  );
+
+  return (
+    <>
+      {appTitle}
+      {statistic}
+    </>
+  );
 }
 
 export default Budget;
