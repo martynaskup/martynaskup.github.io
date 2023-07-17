7830de2715aa92399e59cf271a6bcfecff9c5781
Martyna Skup <martyna.skup@yahoo.de>
2023-07-17 21:31:22 +0200

    Update the project overview with the description and tech stack info about the budget app

--- src/pages/ProjectsPage/Projects/NewProjects/Overview.styles.tsx
+++ src/pages/ProjectsPage/Projects/NewProjects/Overview.styles.tsx
@@ -17,8 +17,10 @@ export const StyledCard = styled(Card)`
   padding: 8px;
   margin: 50px auto 0;
   border-color: #001529;
+  text-align: left;
   .ant-card-head-title {
     font-size: 24px;
+    text-align: center;
   }
 
   img {
@@ -26,6 +28,7 @@ export const StyledCard = styled(Card)`
     padding-bottom: 16px;
     border-bottom: 1px #001529 dotted;
   }
+
   .ant-card-meta {
     margin-bottom: 16px;
   }
--- src/pages/ProjectsPage/Projects/NewProjects/Overview.tsx
+++ src/pages/ProjectsPage/Projects/NewProjects/Overview.tsx
@@ -5,20 +5,39 @@ import { Link } from 'react-router-dom';
 import paths, { generatePathToPage } from '../../../../routes/paths';
 import { ArrowRightOutlined } from '@ant-design/icons';
 import NewProjectPlaceholder from './NewProjectPlaceholder';
+import { Typography } from 'antd';
 
 function Overview() {
   return (
-    <StyledCarousel
-      dotPosition="top"
-      dots={true}
-      style={{ textAlign: 'center' }}
-    >
+    <StyledCarousel dotPosition="top" dots={true}>
       <div>
         <ProjectCard
           cover={<img alt="budget app screenshot" src={budgetImage} />}
-          techStack="HTML, CSS, React, styled components, Ant Design... "
+          techStack="Typescript, React, React Hooks, Ant Design, styled components, Ant Design (v4), uuid library (v4)"
           title="Budget App"
-          children="With this app, you can track your monthly expenses and incomes."
+          children={
+            <>
+              <Typography.Text>
+                With this app, you can track your monthly expenses and incomes.
+              </Typography.Text>
+              <ul>
+                <li>You can add, edit and delete your incomes and outcomes.</li>
+                <li>
+                  The app will show you the total incomes, total outcomes and
+                  the total amount left in the month. It will show also how many
+                  days are left until the end of the month.
+                </li>
+                <li>
+                  The app contains budget examples by design, as it is a demo
+                  version.
+                </li>
+                <li>
+                  As it is a demo app, data is not stored in any external
+                  database nor in the local storage.
+                </li>
+              </ul>
+            </>
+          }
           link={
             <Link to={generatePathToPage(paths.projectsPaths.budget)}>
               View <ArrowRightOutlined />
