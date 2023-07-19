f056ae3533f270c1128023f54de966a757b85009
Martyna Skup <martyna.skup@yahoo.de>
2023-07-19 15:02:41 +0200

    Update links on the home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import { StyledStepsSpace, StyledStepsTypographyText } from './Home.styles';
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
+import paths from '../../routes/paths';
 
 function HomePage() {
   const { lg } = useBreakpoint();
@@ -20,19 +21,20 @@ function HomePage() {
         <StyledStepsSpace align="start" wrap size={lg ? 48 : 16}>
           <StyledStepsTypographyText>
             I encourage you to take a look at a few information{' '}
-            <Link to="/about">about me</Link> first. You will find out who I am,
-            why I code and what I do when it's not the coding-time.
+            <Link to={paths.about}>about me</Link> first. You will find out who
+            I am, why I code and what I do when it's not the coding-time.
           </StyledStepsTypographyText>
           <StyledStepsTypographyText>
-            Then check out some of my <Link to="/projects">projects</Link> build
-            with different tech stack. It will give you an overview of some of
-            my skills.
+            Then check out some of my{' '}
+            <Link to={paths.projectsPaths.base}>projects</Link> build with
+            different tech stack. It will give you an overview of some of my
+            skills.
           </StyledStepsTypographyText>
           <StyledStepsTypographyText>
             I left also a few options for you to{' '}
-            <Link to="/contact">contact me</Link>. Feel free to use them to let
-            me know you would like to cooperate with me, have a question or tell
-            me something nice.
+            <Link to={paths.contactMe}>contact me</Link>. Feel free to use them
+            to let me know you would like to cooperate with me, have a question
+            or tell me something nice.
           </StyledStepsTypographyText>
         </StyledStepsSpace>
       </Space>
