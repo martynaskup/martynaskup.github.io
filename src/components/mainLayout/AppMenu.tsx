058e8417b21bc0572539ff7a08831bf5a56aa382
Martyna Skup <martyna.skup@yahoo.de>
2023-07-20 07:52:21 +0200

    Refactor AppMenu for better code readability

--- src/components/mainLayout/AppMenu.tsx
+++ src/components/mainLayout/AppMenu.tsx
@@ -7,14 +7,14 @@ import { menuItems } from './AppMenuItems';
 function AppMenu() {
   const location = useLocation();
 
-  const defaultSelectedAndOpenKeys = getMenuKeys(location.pathname.slice(1));
+  const menuKeysBasedOnLocation = getMenuKeys(location.pathname.slice(1));
 
   const [openMenuKeys, setOpenMenuKeys] = useState(
-    () => defaultSelectedAndOpenKeys.selectedKeys
+    () => menuKeysBasedOnLocation.selectedKeys
   );
 
   useEffect(() => {
-    const openKeys = getMenuKeys(location.pathname.slice(1)).openKeys;
+    const openKeys = menuKeysBasedOnLocation.openKeys;
     setOpenMenuKeys(openKeys);
   }, [location.pathname]);
 
@@ -23,7 +23,7 @@ function AppMenu() {
       theme="dark"
       mode="inline"
       items={menuItems}
-      selectedKeys={getMenuKeys(location.pathname.slice(1)).selectedKeys}
+      selectedKeys={menuKeysBasedOnLocation.selectedKeys}
       defaultOpenKeys={openMenuKeys}
       openKeys={openMenuKeys}
       onOpenChange={(e) => setOpenMenuKeys(e)}
