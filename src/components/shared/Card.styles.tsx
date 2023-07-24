3f62510f33b08df163740c27ee4b04bb763656b9
Martyna Skup <martyna.skup@yahoo.de>
2023-07-24 21:25:40 +0200

    Update padding in the Card component

--- src/components/shared/Card.styles.tsx
+++ src/components/shared/Card.styles.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { Card } from 'antd';
 
 export const StyledCard = styled(Card)`
+  padding: 8px;
   border: 1px ridge rgba(0, 21, 41, 0.3);
   border-radius: 2px;
   box-shadow: 4px 4px 8px rgba(0, 21, 41, 0.2);
