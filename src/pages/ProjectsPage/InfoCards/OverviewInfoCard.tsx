3f519d76620e59f891e80924c4ee780c6c5e2916
Martyna Skup <martyna.skup@yahoo.de>
2023-07-24 17:20:02 +0200

    Use Text component in projects overview page

--- src/pages/ProjectsPage/InfoCards/OverviewInfoCard.tsx
+++ src/pages/ProjectsPage/InfoCards/OverviewInfoCard.tsx
@@ -1,6 +1,7 @@
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
-import { Space, Typography } from 'antd';
+import { Space } from 'antd';
 import ProjectCard from '../../../components/shared/ProjectCard';
+import Text from '../../../components/shared/Text';
 
 function OverviewInfoCard() {
   const { sm } = useBreakpoint();
@@ -11,22 +12,20 @@ function OverviewInfoCard() {
         title="Curious about my coding skills?"
         children={
           <Space direction="vertical" size="middle">
-            <Typography.Text>
-              In this section, I would like to present a few projects to you.
-            </Typography.Text>
-            <Typography.Text>
-              Some of them are older and are the result of my learning process
-              from when I was working on becoming a front-end developer.
-            </Typography.Text>
-            <Typography.Text>
-              The others are newer, created this year. I utilized the skills and
+            <Text children="In this section, I would like to present a few projects to you." />
+            <Text
+              children="Some of them are older and are the result of my learning process
+              from when I was working on becoming a front-end developer."
+            />
+            <Text
+              children="The others are newer, created this year. I utilized the skills and
               technologies developed and learned over the last 3 years while
-              working as a front-end developer.
-            </Typography.Text>
-            <Typography.Text>
-              To explore the projects, please go to the Menu, navigate to the
-              Projects submenu, and choose which project you would like to view.
-            </Typography.Text>
+              working as a front-end developer."
+            />
+            <Text
+              children="To explore the projects, please go to the Menu, navigate to the
+              Projects submenu, and choose which project you would like to view."
+            />
           </Space>
         }
       />
