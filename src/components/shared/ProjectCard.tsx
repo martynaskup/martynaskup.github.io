9c6dacee794e9726a28ad03fb20a79289e496601
Martyna Skup <martyna.skup@yahoo.de>
2023-07-24 17:17:41 +0200

    Use Text component in Budget app info page and in ProjectCard

--- src/components/shared/ProjectCard.tsx
+++ src/components/shared/ProjectCard.tsx
@@ -1,7 +1,8 @@
-import { Button, Card, CardProps, Typography } from 'antd';
+import { Button, Card, CardProps } from 'antd';
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import { ReactNode } from 'react';
 import { StyledCard } from './ProjectCard.styles';
+import Text from './Text';
 
 type ProjectCardProps = {
   techStack?: string;
@@ -19,7 +20,7 @@ function ProjectCard({
   textAlign,
   ...props
 }: ProjectCardProps) {
-  const { sm, md } = useBreakpoint();
+  const { sm, md, xl, xxl } = useBreakpoint();
   const cardWidth = md ? '600px' : sm ? '400px' : '300px';
 
   return (
@@ -28,8 +29,13 @@ function ProjectCard({
       extra={<Button key={1}>{link}</Button>}
       style={{ width: width ?? cardWidth, textAlign: textAlign ?? 'left' }}
     >
-      {techStack ? <Card.Meta description={`TechStack: ${techStack}`} /> : null}
-      <Typography.Text>{children}</Typography.Text>
+      {techStack ? (
+        <Card.Meta
+          description={`TechStack: ${techStack}`}
+          style={{ fontSize: xxl ? '17px' : xl ? '16px' : '14px' }}
+        />
+      ) : null}
+      <Text children={children} />
     </StyledCard>
   );
 }
--- src/pages/ProjectsPage/InfoCards/BudgetInfoCard.tsx
+++ src/pages/ProjectsPage/InfoCards/BudgetInfoCard.tsx
@@ -1,10 +1,10 @@
 import ProjectCard from '../../../components/shared/ProjectCard';
 import budgetImage from '../../../assets/images/projectScreenshots/budget.png';
-import { Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import paths, { generatePathToPage } from '../../../routes/paths';
 import { ArrowRightOutlined } from '@ant-design/icons';
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
+import Text from '../../../components/shared/Text';
 
 function BudgetInfoCard() {
   const { sm } = useBreakpoint();
@@ -16,30 +16,33 @@ function BudgetInfoCard() {
       width={sm ? '500px' : '300px'}
       children={
         <>
-          <Typography.Text>
-            With this app, you can track your expenses and incomes for the
-            current month
-          </Typography.Text>
-          <ul>
-            <li>You can add, edit, and delete your incomes and expenses.</li>
-            <li>
-              The app restricts adding incomes or expenses with dates before or
-              after the current month.
-            </li>
-            <li>
-              It will display the total incomes, total expenses, and the
-              remaining balance for the month. Additionally, the app will show
-              the number of days left until the end of the month.
-            </li>
-            <li>
-              The app includes budget examples as part of its design, serving as
-              a demo version
-            </li>
-            <li>
-              Being a demo app, data is not stored in any external database or
-              local storage.
-            </li>
-          </ul>
+          <Text children="With this app, you can track your expenses and incomes for the current month" />
+          <Text
+            children={
+              <ul>
+                <li>
+                  You can add, edit, and delete your incomes and expenses.
+                </li>
+                <li>
+                  The app restricts adding incomes or expenses with dates before
+                  or after the current month.
+                </li>
+                <li>
+                  It will display the total incomes, total expenses, and the
+                  remaining balance for the month. Additionally, the app will
+                  show the number of days left until the end of the month.
+                </li>
+                <li>
+                  The app includes budget examples as part of its design,
+                  serving as a demo version
+                </li>
+                <li>
+                  Being a demo app, data is not stored in any external database
+                  or local storage.
+                </li>
+              </ul>
+            }
+          />
         </>
       }
       link={
