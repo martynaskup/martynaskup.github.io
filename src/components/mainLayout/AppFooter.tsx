95be94591ea09dbad7358db2be7d36c48d091e8f
Martyna Skup <martyna.skup@yahoo.de>
2023-07-24 17:39:26 +0200

    Use Text component in the app footer

--- src/components/mainLayout/AppFooter.tsx
+++ src/components/mainLayout/AppFooter.tsx
@@ -1,10 +1,11 @@
-import { Button, Space, Typography } from 'antd';
+import { Button, Space } from 'antd';
 import {
   GithubOutlined,
   LinkedinOutlined,
   MailOutlined,
 } from '@ant-design/icons';
 import { StyledAppFooter } from './MainLayout.styles';
+import Text from '../shared/Text';
 
 function AppFooter() {
   const iconColor: string = 'rgba(0, 0, 0, 0.88)';
@@ -12,33 +13,37 @@ function AppFooter() {
   return (
     <StyledAppFooter>
       <Space size="small">
-        <Typography.Text>
-          Created with Ant Design ©2023 by Martyna Skup (
-          <Button
-            type="link"
-            href="https://github.com/snikerslala"
-            target="_blank"
-            rel="noopener"
-            size="small"
-            icon={<GithubOutlined style={{ color: iconColor }} />}
-          />
-          <Button
-            type="link"
-            href="https://www.linkedin.com/in/martynaskup/"
-            target="_blank"
-            rel="noopener"
-            size="small"
-            icon={<LinkedinOutlined style={{ color: iconColor }} />}
-          />
-          <Button
-            type="link"
-            href="mailto: martyna.skup@yahoo.de"
-            rel="noopener"
-            size="small"
-            icon={<MailOutlined style={{ color: iconColor }} />}
-          />
-          )
-        </Typography.Text>
+        <Text
+          children={
+            <>
+              Created with Ant Design ©2023 by Martyna Skup (
+              <Button
+                type="link"
+                href="https://github.com/snikerslala"
+                target="_blank"
+                rel="noopener"
+                size="small"
+                icon={<GithubOutlined style={{ color: iconColor }} />}
+              />
+              <Button
+                type="link"
+                href="https://www.linkedin.com/in/martynaskup/"
+                target="_blank"
+                rel="noopener"
+                size="small"
+                icon={<LinkedinOutlined style={{ color: iconColor }} />}
+              />
+              <Button
+                type="link"
+                href="mailto: martyna.skup@yahoo.de"
+                rel="noopener"
+                size="small"
+                icon={<MailOutlined style={{ color: iconColor }} />}
+              />
+              )
+            </>
+          }
+        />
       </Space>
     </StyledAppFooter>
   );
