06fc6554ba77bf8249d78cb45e9821b9ee8caf4e
Martyna Skup <martyna.skup@yahoo.de>
2023-07-24 20:10:49 +0200

    Use Title component in home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,20 @@
-import { Space, Typography } from 'antd';
+import { Space } from 'antd';
 import React from 'react';
 import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import HomeSteps from './HomeSteps';
 import MyImage from '../../components/MyImage';
 import { StyledSpace } from './Home.styles';
 import Card from '../../components/shared/Card';
+import Title from '../../components/shared/Title';
 
 function HomePage() {
   const { md } = useBreakpoint();
 
   return (
     <StyledSpace direction="vertical" size="middle" align="center">
-      <Typography.Title>Hello! I'm Martyna, React developer</Typography.Title>
-      <Typography.Title level={2}>
-        Welcome to my portfolio website!
-      </Typography.Title>
+      <Title level={1} children="Hello! I'm Martyna, React developer" />
+      <Title level={2} children="Welcome to my portfolio website!" />
+
       <Card width={md ? '700px' : '300px'}>
         <Space
           direction={md ? 'horizontal' : 'vertical'}
